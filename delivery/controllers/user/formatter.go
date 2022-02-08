7cd57a291583510f9b54709b12e630faabaf0545
Ade Mawan <ademawan1210@gmail.com>
2022-02-08 21:03:06 +0700

    testing controllers user

--- delivery/controllers/user/formatter.go
+++ delivery/controllers/user/formatter.go
@@ -17,10 +17,20 @@ type RegisterRequestFormat struct {
 	Password string `json:"password" form:"password"`
 }
 
-type GetUserResponseFormat struct {
+type RegisterUserResponseFormat struct {
+	Code    int           `json:"code"`
+	Message string        `json:"message"`
+	Data    entities.User `json:"data"`
+}
+type GetUsersResponseFormat struct {
 	Code    int             `json:"code"`
 	Message string          `json:"message"`
 	Data    []entities.User `json:"data"`
+}
+type GetUserResponseFormat struct {
+	Code    int           `json:"code"`
+	Message string        `json:"message"`
+	Data    entities.User `json:"data"`
 }
 
 type UpdateRequestFormat struct {
@@ -28,4 +38,15 @@ type UpdateRequestFormat struct {
 	Nama     string `json:"nama" form:"nama"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
+}
+
+type UpdateResponseFormat struct {
+	Code    int           `json:"code"`
+	Message string        `json:"message"`
+	Data    entities.User `json:"data"`
+}
+type DeleteResponseFormat struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
--- delivery/controllers/user/user_test.go
+++ delivery/controllers/user/user_test.go
@@ -3,6 +3,7 @@ package user
 import (
 	"Project-REST-API/entities"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,41 +13,176 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	res := httptest.NewRecorder()
-	context := e.NewContext(req, res)
-	context.SetPath("/users")
 
-	userController := New(&MockUserRepository{})
-	userController.Get()(context)
+	t.Run("UserGet", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+		context := e.NewContext(req, res)
+		context.SetPath("/users")
 
-	response := GetUserResponseFormat{}
+		userController := New(MockUserRepository{})
+		userController.Get()(context)
 
-	json.Unmarshal([]byte(res.Body.Bytes()), &response)
+		var response GetUsersResponseFormat
 
-	assert.Equal(t, 200, response.Code)
-	assert.Equal(t, "Adlan", response.Data[0].Nama)
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+		assert.Equal(t, response.Data[0].Nama, "Adlan")
+		//
+	})
+	t.Run("GetUser", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+
+		context := e.NewContext(req, res)
+		context.SetPath("/users")
+
+		falseUserController := New(MockFalseUserRepository{})
+		falseUserController.Get()(context)
+
+		var response GetUserResponseFormat
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+		assert.Equal(t, response.Message, "There is some error on server")
+	})
+
+	// e := echo.New()
+	// req := httptest.NewRequest(http.MethodGet, "/", nil)
+	// res := httptest.NewRecorder()
+	// context := e.NewContext(req, res)
+	// context.SetPath("/users")
+
+	// userController := New(&MockUserRepository{})
+	// userController.Get()(context)
+
+	// response := GetUserResponseFormat{}
+
+	// json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+	// assert.Equal(t, 200, response.Code)
+	// assert.Equal(t, "Adlan", response.Data[0].Nama)
 
 }
 
 func TestGetById(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	res := httptest.NewRecorder()
-	context := e.NewContext(req, res)
-	context.SetPath("/users/:id")
+	t.Run("GetById", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+		context := e.NewContext(req, res)
+		context.SetPath("/users/:id")
 
-	userController := New(&MockUserRepository{})
-	userController.Get()(context)
+		userController := New(&MockUserRepository{})
+		userController.GetById()(context)
 
-	response := GetUserResponseFormat{}
+		response := GetUserResponseFormat{}
 
-	json.Unmarshal([]byte(res.Body.Bytes()), &response)
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
 
-	assert.Equal(t, 200, response.Code)
-	assert.Equal(t, "Adlan", response.Data[0].Nama)
+		assert.Equal(t, 200, response.Code)
+		assert.Equal(t, "Adlan", response.Data.Nama)
 
+	})
+	t.Run("Eror GetById", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+
+		context := e.NewContext(req, res)
+		context.SetPath("/users/:id")
+
+		falseUserController := New(MockFalseUserRepository{})
+		falseUserController.GetById()(context)
+
+		var response GetUserResponseFormat
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+		assert.Equal(t, response.Message, "not found")
+	})
+
+}
+
+func TestUserRegister(t *testing.T) {
+	t.Run("UserRegister", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		res := httptest.NewRecorder()
+		context := e.NewContext(req, res)
+		context.SetPath("/users")
+
+		userController := New(&MockUserRepository{})
+		userController.UserRegister()(context)
+
+		response := RegisterUserResponseFormat{}
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, 201, response.Code)
+		assert.Equal(t, "Adlan", response.Data.Nama)
+		assert.Equal(t, "adlan@adlan.com", response.Data.Email)
+		assert.Equal(t, "adlan123", response.Data.Password)
+
+	})
+	t.Run("Eror UserRegister", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		res := httptest.NewRecorder()
+		context := e.NewContext(req, res)
+		context.SetPath("/users")
+
+		userController := New(&MockFalseUserRepository{})
+		userController.UserRegister()(context)
+
+		response := RegisterUserResponseFormat{}
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, 500, response.Code)
+		assert.Equal(t, "There is some error on server", response.Message)
+
+	})
+
+}
+func TestUpdate(t *testing.T) {
+	t.Run("Update", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodPut, "/", nil)
+		res := httptest.NewRecorder()
+		context := e.NewContext(req, res)
+		context.SetPath("/users/:id")
+
+		userController := New(&MockUserRepository{})
+		userController.Update()(context)
+
+		response := UpdateResponseFormat{}
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, 200, response.Code)
+		assert.Equal(t, "Adlan", response.Data.Nama)
+
+	})
+}
+func TestDelete(t *testing.T) {
+	t.Run("UserRegister", func(t *testing.T) {
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodDelete, "/", nil)
+		res := httptest.NewRecorder()
+		context := e.NewContext(req, res)
+		context.SetPath("/users/:id")
+
+		userController := New(&MockUserRepository{})
+		userController.Delete()(context)
+
+		response := DeleteResponseFormat{}
+
+		json.Unmarshal([]byte(res.Body.Bytes()), &response)
+
+		assert.Equal(t, 200, response.Code)
+		assert.Equal(t, nil, response.Data)
+
+	})
 }
 
 type MockUserRepository struct{}
@@ -75,4 +211,25 @@ func (m MockUserRepository) Update(userId int, newUser entities.User) (entities.
 
 func (m MockUserRepository) Delete(userId int) error {
 	return nil
+}
+
+type MockFalseUserRepository struct{}
+
+func (m MockFalseUserRepository) Get() ([]entities.User, error) {
+	return nil, errors.New("False User Object")
+}
+func (m MockFalseUserRepository) GetById(userId int) (entities.User, error) {
+	return entities.User{}, errors.New("False Get Object")
+}
+func (m MockFalseUserRepository) UserRegister(newUser entities.User) (entities.User, error) {
+	return entities.User{}, errors.New("False Register Object")
+}
+func (m MockFalseUserRepository) Login(data entities.User) (entities.User, error) {
+	return entities.User{}, errors.New("False Login Object")
+}
+func (m MockFalseUserRepository) Update(userId int, newUser entities.User) (entities.User, error) {
+	return entities.User{}, errors.New("False Update Object")
+}
+func (m MockFalseUserRepository) Delete(userId int) error {
+	return errors.New("False Delete Object")
 }
